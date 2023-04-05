50c674a535f0b86702d064aaf547d09e11294db9
Oussama ELJabbari <contact@ojpro.me>
2023-04-05 09:23:17 +0000

    fix bug related to selecting the correct app

--- main.py
+++ main.py
@@ -36,22 +36,23 @@ def displayAppsMenu(apps):
         print("\n\n++++++++++++++++ Results ++++++++++++++++\n")
         for i in range(len(apps)):
             app = apps[i]
-            print("["+(str(app[0]))+"] => "+ app[1])
+            print("["+(str(i+1))+"] => "+ app[1] + " - " + str(apps[i][0]))
 
-        app_id = 0
-
+        app = []
         if len(apps) >= 1:
             print("\n------------- Which App -------------\n")
             app_id = int(prompt("Choice App : "))
+            app = apps[app_id-1]
+
         elif len(apps) == 1:
-            app_id = apps[0][0]
+            app = apps[0]
         else :
             print("No apps with this name")
             exit()
 
         # ask for options
         print("\n--------------------- App Menu ---------------------\n")
-        print("================= " + apps[app_id-1][1] + " ===============")
+        print("================= " + app[1] + " ===============")
         print("1. See Password.")
         print("2. Change Password.")
         print("3. Random Password.")
@@ -60,14 +61,13 @@ def displayAppsMenu(apps):
         
         # do what need to be done
         if option == 1:
-            password = search("SELECT password FROM applications WHERE `id`="+str(app_id))
-            print("\n\n\n"+password[0][0] + "\n\n")
+            selected_app = search("SELECT username,password FROM applications WHERE `id`="+str(app[0]))
+            print("\n\n\n------ Password for user  [ "+selected_app[0][0]  +" ]  is : [ "+selected_app[0][1] + " ]\n\n")
         elif option == 2:
             password = prompt("New Password : ")
-            insert("UPDATE applications SET `password`='"+password+"' WHERE `id`="+str(app_id -1))
+            insert("UPDATE applications SET `password`='"+password+"' WHERE `id`="+str(app[0]))
             print("Password Changed\n\n")
         elif option == 3:
-            app = apps[app_id -1]
             password = randomPassword()
             insert("UPDATE applications SET `password`='"+password+"' WHERE `id`="+str(app[0]))
             # print success message
@@ -76,13 +76,13 @@ def displayAppsMenu(apps):
         elif option == 4:
             remove = prompt("Are You Sure You wanna Delete it [Y]es / [N]o : ")
             if remove == 'y':
-                insert("DELETE FROM applications WHERE `id`="+str(app_id))
+                insert("DELETE FROM applications WHERE `id`="+str(app[0]))
                 print("Application Deleted.\n\n")
             displayMenu()
         
     # create the applications table
 def init():
-    execute("CREATE TABLE IF NOT EXISTS applications(id INTEGER primary key ,name TEXT, password TEXT, status TEXT, update_at DATETIME);")
+    execute("CREATE TABLE IF NOT EXISTS applications(id INTEGER primary key ,name TEXT, username TEXT, password TEXT, status TEXT, update_at DATETIME);")
 
 
 # generate random password
@@ -108,10 +108,11 @@ def displayMenu():
             # ask for app name and password
             print("\n------------- Add New Application -------------\n")
             name = prompt("Application Name : ")
+            username = prompt("Username for ["+ name +"] : ")
             password = prompt("Password for ["+ name +"] : ")
 
             # add the new application
-            res = insert("INSERT INTO applications(`name`,`password`,`update_at`) VALUES('"+name+"','"+password+"','"+time.ctime()+"')")
+            res = insert("INSERT INTO applications(`name`,`username`,`password`,`update_at`) VALUES('"+name+"','"+username+"','"+password+"','"+time.ctime()+"')")
 
             if res is None:
                 print("\n\n ##### Application Added Successfully. ##### \n")
@@ -126,7 +127,7 @@ def displayMenu():
         print("\n------------- Search 4 Application -------------\n")
         app_name = prompt("Search for : ",False)
 
-        apps = search("SELECT * FROM applications WHERE `name` LIKE '%"+app_name+"%'")
+        apps = search("SELECT * FROM applications WHERE `name` LIKE '%"+app_name+"%' OR `username`  LIKE '%"+app_name+"%' ")
 
         displayAppsMenu(apps)
     
